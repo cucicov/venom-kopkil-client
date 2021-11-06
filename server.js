28d9fdb9b85d394dce2649149a05ad0f02102204
Dorin Cucicov <cucicov@gmail.com>
2021-11-07 00:27:10 +0200

    fix

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var app = express()
-import AxiosPlugin from 'vue-axios-cors';
 
-app.use(AxiosPlugin)
 app.use(serveStatic(path.join(__dirname, 'dist')))
 var port = process.env.PORT
 app.listen(port, () => {
