d1903d1d682989cde805632931cdb5248bec21b6
Dorin Cucicov <cucicov@gmail.com>
2021-11-07 00:33:57 +0200

    added cors..

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var app = express()
+var cors = require('cors')
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(cors())
+
 var port = process.env.PORT
 app.listen(port, () => {
   console.log(port)
