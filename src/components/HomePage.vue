c9d19084bf4983160fb556ef12e53aa03207a15a
Dorin Cucicov <cucicov@gmail.com>
2022-11-10 09:42:11 +0200

    expansion

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -47,7 +47,7 @@
           evoluat oamenii în neolitic? Să începem cu începutul.
         </div>
         <v-expansion-panels accordion multiple v-model="panelcontentvideos">
-          <v-expansion-panel expand="false" elevation-0>
+          <v-expansion-panel expand elevation-0>
             <div class="header-top-line"></div>
             <v-expansion-panel-header>
               Sezonul I
@@ -134,7 +134,7 @@ export default {
   },
   data() {
     return {
-      panelcontentvideos: [0], // keep only first panel expanded.
+      panelcontentvideos: [1], // keep only first panel expanded.
       activeClass: 1,
       newsletter_email: '',
       snackbar: false,
